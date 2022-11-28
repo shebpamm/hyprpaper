0e886f1fc6ef48ef574316797816f2203dfe7e11
shebpamm <erik@opcode.xyz>
2022-11-29 00:47:20 +0200

    remove mkDate coz it's not working and I'm too bad at fixing things

--- flake.nix
+++ flake.nix
@@ -8,22 +8,16 @@
     nixpkgs,
     ...
   }: let
-    inherit (nixpkgs) lib;
-    genSystems = lib.genAttrs [
+    genSystems = nixpkgs.lib.genAttrs [
       # Add more systems if they are supported
       "x86_64-linux"
     ];
     pkgsFor = nixpkgs.legacyPackages;
-    mkDate = longDate: (lib.concatStringsSep "-" [
-      (__substring 0 4 longDate)
-      (__substring 4 2 longDate)
-      (__substring 6 2 longDate)
-    ]);
   in {
     overlays.default = _: prev: rec {
       hyprpaper = prev.callPackage ./nix/default.nix {
         stdenv = prev.gcc12Stdenv;
-        version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+        version = "0.pre" + "+date=" + (self.lastModifiedDate or "19700101") + "_" + (self.shortRev or "dirty");
         inherit (prev.xorg) libXdmcp;
       };
       hyprpaper-debug = hyprpaper.override {debug = true;};
